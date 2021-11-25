0dda2e6fdb7873649b89e53d7b8ccf64521182a1
Nikhil Kurian Joseph <nikhil.kurian.n3@gmail.com>
2021-11-25 16:44:15 +0000

    Updates 1) Refactor

--- gpu/study/report.py
+++ gpu/study/report.py
@@ -2,7 +2,7 @@ import datetime
 
 import matplotlib.pyplot as plt
 import numpy as np
-from tqdm import trange
+from tqdm import tqdm
 
 from matrix_element_sum import gpu_matrix_sum
 from matrix_product import gpu_matrix_product
@@ -36,13 +36,17 @@ def simple_product(a, b):
 
 
 def get_report(cpu_func, gpu_func, title, matrix_sizes=(10, 1000, 10)):
+    matrices_list = [
+        np.random.randint(matrix_size, size=[2, matrix_size, matrix_size])
+        for matrix_size in range(*matrix_sizes)
+    ]
     cpu_report = [
-        timeit(cpu_func, *np.random.randint(matrix_size, size=[2, matrix_size, matrix_size]))
-        for matrix_size in trange(*matrix_sizes)
+        timeit(cpu_func, *matrices)
+        for matrices in tqdm(matrices_list)
     ]
     gpu_report = [
-        timeit(gpu_func, *np.random.randint(matrix_size, size=[2, matrix_size, matrix_size]))
-        for matrix_size in trange(*matrix_sizes)
+        timeit(gpu_func, *matrices)
+        for matrices in tqdm(matrices_list)
     ]
 
     plt.plot(range(*matrix_sizes), cpu_report, label='CPU')
@@ -55,7 +59,7 @@ def get_report(cpu_func, gpu_func, title, matrix_sizes=(10, 1000, 10)):
     plt.show()
 
 
-# get_report(simple_sum, gpu_matrix_sum, 'Matrix Sum', matrix_sizes=(10, 1500, 10))
+get_report(simple_sum, gpu_matrix_sum, 'Matrix Sum', matrix_sizes=(10, 1500, 10))
 # get_report(simple_product, gpu_matrix_product, 'Matrix Product', matrix_sizes=(10, 250, 10))
-get_report(np.matmul, gpu_matrix_product, 'Matrix Product')
+# get_report(np.matmul, gpu_matrix_product, 'Matrix Product')
 
